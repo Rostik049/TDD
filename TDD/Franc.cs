50a5670d6a6be01caf30bb81e6286cfd4b8d30b8
Rostik <rostislav.prodan@gmail.com>
2023-11-20 21:30:57 +0200

    franc test

---
+++ TDD/Franc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TDD
+{
+    public class Franc
+    {
+        private int amount;
+        public Franc(int amount) { this.amount = amount; }
+        public Franc times(int multiplier)
+        {
+            return new Franc(amount * multiplier);
+        }
+        public bool Equals(Object o)
+        {
+            Franc franc = (Franc)o;
+            return amount == franc.amount;
+        }
+    }
+}
